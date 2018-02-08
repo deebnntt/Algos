5aad54efe2ca948ce4a2e0dbca41a060945cfab1
deebnntt <daniellebennettchan@gmail.com>
2018-02-08 16:56:30 -0500

    solved pyramid

--- steps/index.js
+++ steps/index.js
@@ -17,11 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i=1; i<=n; i++) {
-    let output = `${'#'.repeat(i)}`+`${' '.repeat(n-i)}`
-    console.log(output)
+// function steps(n) {
+//   for (let i=1; i<=n; i++) {
+//     let output = `${'#'.repeat(i)}`+`${' '.repeat(n-i)}`
+//     console.log(output)
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+//
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair)
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return
   }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+      stair += '#'
+    } else {
+      stair += ' ';
+  }
+
+  steps(n, row, stair)
 }
 
 module.exports = steps;
