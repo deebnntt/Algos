c2560d95fed8c59a765260cedcf8293c8799c54a
deebnntt <daniellebennettchan@gmail.com>
2018-02-08 13:07:02 -0500

    solved capitalize

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,19 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  let arrA = toArray(stringA).sort()
-  let arrB = toArray(stringB).sort()
+// function anagrams(stringA, stringB) {
+//   let arrA = toArray(stringA).sort()
+//   let arrB = toArray(stringB).sort()
+//
+//   if (arrA.toString() !== arrB.toString()) {
+//     return false
+//   }
+//
+//   return true
+// }
+//
+// function toArray(string) {
+//   return string.toLowerCase().replace(/[^\w]/g, '').split('')
+// }
 
-  if (arrA.toString() !== arrB.toString()) {
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA)
+  const bCharMap = buildCharMap(stringB)
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
     return false
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false
+    }
   }
 
   return true
 }
 
-function toArray(string) {
-  return string.toLowerCase().replace(/[^\w]/g, '').split('')
+function buildCharMap(str) {
+  const charMap = {}
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
 }
 
 module.exports = anagrams;
